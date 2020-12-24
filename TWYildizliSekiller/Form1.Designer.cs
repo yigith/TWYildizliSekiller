87a80a860336e7e2e05aa41f237fba797bfde907
Yigit Haciefendioglu <yigith1@gmail.com>
2020-12-24 22:14:50 +0300

    Add project files.

---
+++ TWYildizliSekiller/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿
+namespace TWYildizliSekiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudSatirAdet = new System.Windows.Forms.NumericUpDown();
+            this.txtSekil1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtYildizKarakter = new System.Windows.Forms.TextBox();
+            this.txtSekil2 = new System.Windows.Forms.TextBox();
+            this.txtSekil4 = new System.Windows.Forms.TextBox();
+            this.txtSekil3 = new System.Windows.Forms.TextBox();
+            this.txtSekil5 = new System.Windows.Forms.TextBox();
+            this.txtSekil10 = new System.Windows.Forms.TextBox();
+            this.txtSekil9 = new System.Windows.Forms.TextBox();
+            this.txtSekil8 = new System.Windows.Forms.TextBox();
+            this.txtSekil7 = new System.Windows.Forms.TextBox();
+            this.txtSekil6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSatirAdet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Satır Sayısı:";
+            // 
+            // nudSatirAdet
+            // 
+            this.nudSatirAdet.Location = new System.Drawing.Point(118, 11);
+            this.nudSatirAdet.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.nudSatirAdet.Name = "nudSatirAdet";
+            this.nudSatirAdet.Size = new System.Drawing.Size(180, 26);
+            this.nudSatirAdet.TabIndex = 1;
+            this.nudSatirAdet.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.nudSatirAdet.ValueChanged += new System.EventHandler(this.nudSatirAdet_ValueChanged);
+            // 
+            // txtSekil1
+            // 
+            this.txtSekil1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSekil1.Location = new System.Drawing.Point(18, 45);
+            this.txtSekil1.Multiline = true;
+            this.txtSekil1.Name = "txtSekil1";
+            this.txtSekil1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSekil1.Size = new System.Drawing.Size(280, 263);
+            this.txtSekil1.TabIndex = 2;
+            this.txtSekil1.WordWrap = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(324, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Yıldız:";
+            // 
+            // txtYildizKarakter
+            // 
+            this.txtYildizKarakter.Location = new System.Drawing.Point(381, 11);
+            this.txtYildizKarakter.Name = "txtYildizKarakter";
+            this.txtYildizKarakter.Size = new System.Drawing.Size(100, 26);
+            this.txtYildizKarakter.TabIndex = 4;
+            this.txtYildizKarakter.Text = "*";
+            this.txtYildizKarakter.TextChanged += new System.EventHandler(this.txtYildizKarakter_TextChanged);
+            // 
+            // txtSekil2
+            // 
+            this.txtSekil2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSekil2.Location = new System.Drawing.Point(304, 45);
+            this.txtSekil2.Multiline = true;
+            this.txtSekil2.Name = "txtSekil2";
+            this.txtSekil2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSekil2.Size = new System.Drawing.Size(280, 263);
+            this.txtSekil2.TabIndex = 5;
+            this.txtSekil2.WordWrap = false;
+            // 
+            // txtSekil4
+            // 
+            this.txtSekil4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSekil4.Location = new System.Drawing.Point(876, 45);
+            this.txtSekil4.Multiline = true;
+            this.txtSekil4.Name = "txtSekil4";
+            this.txtSekil4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSekil4.Size = new System.Drawing.Size(280, 263);
+            this.txtSekil4.TabIndex = 7;
+            this.txtSekil4.WordWrap = false;
+            // 
+            // txtSekil3
+            // 
+            this.txtSekil3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSekil3.Location = new System.Drawing.Point(590, 45);
+            this.txtSekil3.Multiline = true;
+            this.txtSekil3.Name = "txtSekil3";
+            this.txtSekil3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSekil3.Size = new System.Drawing.Size(280, 263);
+            this.txtSekil3.TabIndex = 6;
+            this.txtSekil3.WordWrap = false;
+            // 
+            // txtSekil5
+            // 
+            this.txtSekil5.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSekil5.Location = new System.Drawing.Point(1162, 45);
+            this.txtSekil5.Multiline = true;
+            this.txtSekil5.Name = "txtSekil5";
+            this.txtSekil5.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSekil5.Size = new System.Drawing.Size(280, 263);
+            this.txtSekil5.TabIndex = 8;
+            this.txtSekil5.WordWrap = false;
+            // 
+            // txtSekil10
+            // 
+            this.txtSekil10.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSekil10.Location = new System.Drawing.Point(1162, 314);
+            this.txtSekil10.Multiline = true;
+            this.txtSekil10.Name = "txtSekil10";
+            this.txtSekil10.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSekil10.Size = new System.Drawing.Size(280, 263);
+            this.txtSekil10.TabIndex = 13;
+            this.txtSekil10.WordWrap = false;
+            // 
+            // txtSekil9
+            // 
+            this.txtSekil9.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSekil9.Location = new System.Drawing.Point(876, 314);
+            this.txtSekil9.Multiline = true;
+            this.txtSekil9.Name = "txtSekil9";
+            this.txtSekil9.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSekil9.Size = new System.Drawing.Size(280, 263);
+            this.txtSekil9.TabIndex = 12;
+            this.txtSekil9.WordWrap = false;
+            // 
+            // txtSekil8
+            // 
+            this.txtSekil8.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSekil8.Location = new System.Drawing.Point(590, 314);
+            this.txtSekil8.Multiline = true;
+            this.txtSekil8.Name = "txtSekil8";
+            this.txtSekil8.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSekil8.Size = new System.Drawing.Size(280, 263);
+            this.txtSekil8.TabIndex = 11;
+            this.txtSekil8.WordWrap = false;
+            // 
+            // txtSekil7
+            // 
+            this.txtSekil7.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSekil7.Location = new System.Drawing.Point(304, 314);
+            this.txtSekil7.Multiline = true;
+            this.txtSekil7.Name = "txtSekil7";
+            this.txtSekil7.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSekil7.Size = new System.Drawing.Size(280, 263);
+            this.txtSekil7.TabIndex = 10;
+            this.txtSekil7.WordWrap = false;
+            // 
+            // txtSekil6
+            // 
+            this.txtSekil6.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSekil6.Location = new System.Drawing.Point(18, 314);
+            this.txtSekil6.Multiline = true;
+            this.txtSekil6.Name = "txtSekil6";
+            this.txtSekil6.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtSekil6.Size = new System.Drawing.Size(280, 263);
+            this.txtSekil6.TabIndex = 9;
+            this.txtSekil6.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1463, 600);
+            this.Controls.Add(this.txtSekil10);
+            this.Controls.Add(this.txtSekil9);
+            this.Controls.Add(this.txtSekil8);
+            this.Controls.Add(this.txtSekil7);
+            this.Controls.Add(this.txtSekil6);
+            this.Controls.Add(this.txtSekil5);
+            this.Controls.Add(this.txtSekil4);
+            this.Controls.Add(this.txtSekil3);
+            this.Controls.Add(this.txtSekil2);
+            this.Controls.Add(this.txtYildizKarakter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSekil1);
+            this.Controls.Add(this.nudSatirAdet);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Şekilli Yıldızlar - 24.12.2020";
+            ((System.ComponentModel.ISupportInitialize)(this.nudSatirAdet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudSatirAdet;
+        private System.Windows.Forms.TextBox txtSekil1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtYildizKarakter;
+        private System.Windows.Forms.TextBox txtSekil2;
+        private System.Windows.Forms.TextBox txtSekil4;
+        private System.Windows.Forms.TextBox txtSekil3;
+        private System.Windows.Forms.TextBox txtSekil5;
+        private System.Windows.Forms.TextBox txtSekil10;
+        private System.Windows.Forms.TextBox txtSekil9;
+        private System.Windows.Forms.TextBox txtSekil8;
+        private System.Windows.Forms.TextBox txtSekil7;
+        private System.Windows.Forms.TextBox txtSekil6;
+    }
+}
+
---
+++ TWYildizliSekiller/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TWYildizliSekiller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SekilleriCiz();
+        }
+
+        private void nudSatirAdet_ValueChanged(object sender, EventArgs e)
+        {
+            SekilleriCiz();
+        }
+
+        private void SekilleriCiz()
+        {
+            int n = (int)nudSatirAdet.Value;
+            string yildiz = txtYildizKarakter.Text;
+            Sekil1(n, yildiz);
+            Sekil2(n, yildiz);
+            Sekil3(n, yildiz);
+            Sekil4(n, yildiz);
+            Sekil5(n, yildiz);
+            Sekil6(n, yildiz);
+            Sekil7(n, yildiz);
+            Sekil8(n, yildiz);
+            Sekil9(n, yildiz);
+        }
+
+        private void Sekil9(int n, string yildiz)
+        {
+            string sonuc = "";
+            string bosluk = Bosluk(yildiz);
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == 1 || i == n)
+                    sonuc += Kere(yildiz, n);
+                else
+                    sonuc += yildiz + Kere(bosluk, n - 2) + yildiz;
+
+                sonuc += Environment.NewLine;
+            }
+
+            txtSekil9.Text = sonuc;
+        }
+
+        private void Sekil8(int n, string yildiz)
+        {
+            string sonuc = "";
+            string bosluk = Bosluk(yildiz);
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    sonuc += (i == j || i + j == n + 1) ? yildiz : bosluk;
+                }
+                sonuc += Environment.NewLine;
+            }
+
+            txtSekil8.Text = sonuc;
+        }
+
+        private void Sekil7(int n, string yildiz)
+        {
+            string sonuc = "";
+            string bosluk = Bosluk(yildiz);
+
+            for (int i = 1; i <= n; i++)
+            {
+                sonuc += Kere(bosluk, n - i);
+                sonuc += yildiz;
+                sonuc += Environment.NewLine;
+            }
+
+            txtSekil7.Text = sonuc;
+        }
+
+        private void Sekil6(int n, string yildiz)
+        {
+            string sonuc = "";
+            string bosluk = Bosluk(yildiz);
+
+            for (int i = 0; i < n; i++)
+            {
+                sonuc += Kere(bosluk, i);
+                sonuc += yildiz;
+                sonuc += Environment.NewLine;
+            }
+
+            txtSekil6.Text = sonuc;
+        }
+
+        private string Bosluk(string yildiz)
+        {
+            return new string(' ', yildiz.Length);
+        }
+
+        private void Sekil5(int n, string yildiz)
+        {
+            string sonuc = "";
+            for (int i = 1; i <= n; i++)
+            {
+                sonuc += Kere(" ", (n - i) * yildiz.Length);
+                sonuc += Kere(yildiz, 2 * i - 1);
+                sonuc += "\r\n";
+            }
+            txtSekil5.Text = sonuc;
+        }
+
+        private void Sekil4(int n, string yildiz)
+        {
+            string sonuc = "";
+            for (int i = 1; i <= n; i++)
+            {
+                sonuc += Kere(" ", (n - i) * yildiz.Length);
+                sonuc += Kere(yildiz, i);
+                sonuc += "\r\n";
+            }
+            txtSekil4.Text = sonuc;
+        }
+
+        private string Kere(string metin, int adet)
+        {
+            string sonuc = "";
+            for (int i = 0; i < adet; i++)
+            {
+                sonuc += metin;
+            }
+            return sonuc;
+        }
+
+        private void Sekil3(int n, string yildiz)
+        {
+            string sonuc = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    sonuc += yildiz + " ";
+                }
+                sonuc += Environment.NewLine;
+            }
+
+            txtSekil3.Text = sonuc;
+        }
+
+        private void Sekil2(int n, string yildiz)
+        {
+            string sonuc = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    sonuc += yildiz;
+                }
+                sonuc += Environment.NewLine;
+            }
+
+            txtSekil2.Text = sonuc;
+        }
+
+        private void Sekil1(int n, string yildiz)
+        {
+            string sonuc = "";
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    sonuc += yildiz;
+                }
+                sonuc +=  "\r\n";
+            }
+
+            txtSekil1.Text = sonuc;
+        }
+
+        private void txtYildizKarakter_TextChanged(object sender, EventArgs e)
+        {
+            SekilleriCiz();
+        }
+    }
+}
